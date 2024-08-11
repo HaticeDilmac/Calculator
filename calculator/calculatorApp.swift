e3d230489a7eb80ee6add6428e861344069c190b
HaticeDilmac <haticedilmac.1011@gmail.com>
2024-08-11 10:58:46 +0300

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by hatice on 11.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
